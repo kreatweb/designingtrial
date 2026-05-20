aae7ef7d7e935d1f8213f04e4ca8b8de1ba7c573
kreatweb <oreoglaze07@gmail.com>
2026-05-20 13:01:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(() => ({
-  base: "/designingtrial/",
+export default defineConfig(({ mode }) => ({
+  base: "/designingtrial-a4cfa49f/",
   server: {
     host: "::",
     port: 8080,
@@ -11,7 +12,7 @@ export default defineConfig(() => ({
       overlay: false,
     },
   },
-  plugins: [react()],
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
